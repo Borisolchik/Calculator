093785d74affd53fcb907de01edf0eecd0503bc9
Olga Boris <olya_boris97@mail.ru>
2023-07-02 00:30:05 +0300

    implementation of the calculator functionality with multiply and divide

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ const input2 = document.getElementById('input2');
 const submitBtn = document.getElementById('submit');
 const plusBtn = document.getElementById('plus');
 const minusBtn = document.getElementById('minus');
+const multiplyBtn = document.getElementById('multiply');
+const divideBtn = document.getElementById('divide');
 let action = '+';
 
 plusBtn.onclick = function () {
     action = '+';
 }
-
 minusBtn.onclick = function () {
     action = '-';
+}
+multiplyBtn.onclick = function () {
+    action = '*';
+}
+divideBtn.onclick = function () {
+    action = '/';
 }
 
 function printResult(result) {
@@ -27,9 +34,13 @@ function computeNumbersWithAction(inp1, inp2, actionSymbol) {
     const num1 = Number(inp1.value);
     const num2 = Number(inp2.value);
     if (actionSymbol === '+') {
-       return num1 + num2;
+        return num1 + num2;
     } else if (actionSymbol === '-') {
         return num1 - num2;
+    } else if (actionSymbol === '*') {
+        return num1 * num2;
+    } else if (actionSymbol === '/') {
+        return num1 / num2;
     }
 }
 
